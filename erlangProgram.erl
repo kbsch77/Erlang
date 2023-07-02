e4c62094cfc482e453eed20908cce5681e1241c4
Kaleb Schauerhamer <97472668+kbsch77@users.noreply.github.com>
2023-07-01 21:53:24 -0600

    comment our errors

---
+++ erlangProgram.erl
@@ -0,0 +1,17 @@
+% simple demo program
+-module(erlangProgram).
+-import(io,[fwrite/1]).
+-export([while/1,while/2, start/0]).
+
+while(L) -> while(L,0).
+while([], Acc) -> Acc;
+
+while([_|T], Acc) ->
+    io:fwrite("~w~n",[Acc]),
+    while(T,Acc+1).
+
+start() ->
+    X = [1,2,3,4],
+    while(X).
+
+    % io:fwrite("Hello, world!\n").
